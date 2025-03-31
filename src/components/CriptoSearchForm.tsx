8ed8cf5ecf9325c78cd5957364d8583f10e6823a
mterrera29 <matiasterrera29@gmail.com>
2025-03-31 14:43:49 -0300

    zod

--- src/components/CriptoSearchForm.tsx
+++ src/components/CriptoSearchForm.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { currencies } from '../data';
 import { useCryptoStore } from '../store';
 import { Pairs } from '../types';
+import ErrorMessage from './ErrorMessage';
 
 export default function CriptoSearchForm() {
   const cryptocurrencies = useCryptoStore((state) => state.cryptocurrencies);
@@ -9,7 +10,8 @@ export default function CriptoSearchForm() {
     currency: '',
     cryptocurrencies: '',
   });
-  const [error, setError] = useState(false);
+
+  const [error, setError] = useState('');
 
   const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setPair({
@@ -21,12 +23,14 @@ export default function CriptoSearchForm() {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (Object.values(pair).includes('')) {
-      setError(true);
+      setError('Todos los campos son obligatorios');
     }
+    setError('');
   };
   console.log(pair);
   return (
     <form className='form' onSubmit={handleSubmit}>
+      {error && <ErrorMessage>{error} </ErrorMessage>}
       <div className='field'>
         <label htmlFor='currency'>Moneda:</label>
         <select
